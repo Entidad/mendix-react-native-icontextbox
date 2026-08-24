268538fa81447962b9c2b8a4f5d40898fe2472da
Jorge Flores <jorge@entidad.io>
2026-08-24 13:48:35 -0700

    Corrected mapping issue with Message input
    Signed-off-by: Jorge Flores <jorge@entidad.io>

--- test/NativeIconTextBoxTestApp/deployment/native/react-native.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactNativePath: 'C:/Program Files/Mendix/11.12.3/modeler/tools/node/node_modules/react-native',
-  dependencies: {
-     'react-native': { root: 'C:/Program Files/Mendix/11.12.3/modeler/tools/node/node_modules/react-native'},
-  }
-};
--- test/NativeIconTextBoxTestApp/deployment/native/widgets/itvisors/nativekeepawake/NativeKeepAwake.android.js
+++
@@ -1,1 +0,0 @@
-import KeepAwake from"@sayem314/react-native-keep-awake";import{createElement}from"react";function NativeKeepAwake(){return createElement(KeepAwake,null)}export{NativeKeepAwake};
